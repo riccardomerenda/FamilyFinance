02b5fe1fb27db92755dfbe6323fee401fd0a7a6e
Riccardo <riccardo-dev@local>
2025-12-20 18:01:36 +0100

    db: Add migration for CostBasis column

--- Migrations/20251220165959_AddCostBasisToInvestments.Designer.cs
+++ Migrations/20251220165959_AddCostBasisToInvestments.Designer.cs
@@ -2,6 +2,7 @@
 using FamilyFinance.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FamilyFinance.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251220165959_AddCostBasisToInvestments")]
+    partial class AddCostBasisToInvestments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
